2cbe738c056b83a4c2f7051c78947fe6235395da
immanuel5568 <immanuelbuelah@gmail.com>
2024-12-16 12:37:30 +0530

    Update methodoverload.java

--- methodoverload.java
+++ methodoverload.java
@@ -1,31 +1,13 @@
-import java.util.*;
-
-class Biotech {
-    void add(int a, int b) {
-        System.out.println("sum: " + (a + b));
-    }
-}
-
-class Computer extends Biotech {
-    void sub(int a, int b) {
-        System.out.println("sub: " + (a - b));
-    }
-}
-
-class Civil extends Biotech {
-    void multi(int a, int b) {
-        System.out.println("multi: " + (a * b));
-    }
-}
-
-class Calculator {
+class methodoverload {
     public static void main(String args[]) {
-        int a = 10;
-        int b = 5;
-        Computer cs = new Computer();
-        Civil eng = new Civil();
-        cs.add(a, b);
-        cs.sub(a, b);
-        eng.multi(a, b);
+        // Predefined value for factorial calculation
+        int n = 5; // Example: Change this to any number for a different factorial
+        int fact = 1;
+
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+
+        System.out.println("The factorial of " + n + " is: " + fact);
     }
 }
